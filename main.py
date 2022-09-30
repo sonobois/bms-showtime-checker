8af77db039f35eb35ea27c733e12ce2fddd5e8e9
Aditeya <aditeya.baral@gmail.com>
2022-09-30 20:00:53 +0530

    Bug fix in fetching movie showtimes

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pytz
+import random
 import logging
 import requests
 import datetime
@@ -14,44 +15,60 @@ logging.basicConfig(
     filemode='w'
 )
 
-def check_movie_showtimes(title, url, date):
-    logging.info(f"Checking showtimes for {title} on {date}")
+def check_movie_showtime_availability(url, date, title):
+    logging.debug(f"Checking for {title}: {url} on {date}")
+    if isinstance(date, str):
+        date = datetime.datetime.strptime(date, "%Y-%m-%d").date()
+    UAS = (
+        "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1",
+        "Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10; rv:33.0) Gecko/20100101 Firefox/33.0",
+        "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.1 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.0 Safari/537.36",
+    )
+    ua = UAS[random.randrange(len(UAS))]
+    headers = {"user-agent": ua}
     try:
-        response = requests.get(url)
+        response = requests.get(url, headers=headers)
         soup = BeautifulSoup(response.text, "html.parser")
+        # title = soup.find("div", class_="sticky-filtertitle").text
         available_dates = soup.find_all("a", class_="date-href")
-        available_dates = [
-            datetime.datetime(
-                date.year, 
-                date.month, 
-                int(list(map(str.strip, available_date.text.strip().split("\n")))[0])
+        for i, available_date in enumerate(available_dates):
+            available_date = available_date.text.strip().split("\n")
+            available_date = list(map(str.strip, available_date))
+            available_date = list(filter(bool, available_date))
+            movie_date, movie_month = available_date[1:]
+            available_dates[i] = datetime.datetime.strptime(
+                f"{movie_date} {movie_month} {date.year}", "%d %b %Y"
             ).date()
-            for available_date in available_dates
-        ]
-        available_dates_string = ', '.join(list(map(str, available_dates)))
-        logging.debug(f"Dates found for {title}: {available_dates_string}")
         check = date in available_dates
         if check:
-            logging.info(f"{title} is showing on {date}")
-        logging.info("Checking showtimes complete")
-        return check
+            logging.info(f"Movie {title} is available on {date}")
+            logging.debug(f"Movie {title} removed from job list")
+            scheduler.remove_job(title)
+        return check, title
     except Exception as e:
-        logging.error(f"Error while looking for showtimes: {e}")
+        logging.error(f"Error while checking movie availability for {url}: {e}")
+        return False, None
 
 if __name__ == "__main__":
     urls = {
-        "Doctor Strange Multiverse of Madness - IMAX 3D": r"https://in.bookmyshow.com/buytickets/doctor-strange-in-the-multiverse-of-madness-bengaluru/movie-bang-ET00326385-MT/20220506",
-        "Doctor Strange Multiverse of Madness - 3D": r"https://in.bookmyshow.com/buytickets/doctor-strange-in-the-multiverse-of-madness-bengaluru/movie-bang-ET00326386-MT/20220506"
+        "Doctor Strange Multiverse of Madness - IMAX 3D": 
+            r"https://in.bookmyshow.com/buytickets/doctor-strange-in-the-multiverse-of-madness-bengaluru/movie-bang-ET00326385-MT/20220506",
+        "Doctor Strange Multiverse of Madness - 3D": 
+            r"https://in.bookmyshow.com/buytickets/doctor-strange-in-the-multiverse-of-madness-bengaluru/movie-bang-ET00326386-MT/20220506"
     }
     date = datetime.datetime(2022, 5, 14, tzinfo=IST).date()
     logging.info(f"Setting up scheduler for checking showtimes on {date}")
     scheduler = BackgroundScheduler()
     for title, url in urls.items():
         scheduler.add_job(
-            func=check_movie_showtimes,
-            args=(title, url, date),
+            func=check_movie_showtime_availability,
+            args=(url, date, title),
             trigger="interval",
-            seconds=60
+            seconds=60,
+            id=title,
         )
     scheduler.start()
     logging.info("Scheduler started")
